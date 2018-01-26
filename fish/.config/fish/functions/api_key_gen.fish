a9c47cc1c64d0098518fc3e7c35aa4d8cb1bace2
Noah Singer <noah.singer@fabricut.com>
2018-01-26 15:51:35 -0600

    shorter version

--- fish/.config/fish/functions/api_key_gen.fish
+++ fish/.config/fish/functions/api_key_gen.fish
@@ -1,6 +1,6 @@
 function api_key_gen
   date | md5 | tr -d '\n'
 
-  # or uuid v4 | tr -d '-' | tr -d '\n' | pbcopy
+  # or uuid v4 | tr -ds '-' '\n' | pbcopy
   # which also requires yarnglobal add uid
 end
