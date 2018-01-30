dd9ac170983f09ce0736b75dfdb426026b161ea2
Noah Singer <noah.singer@fabricut.com>
2018-01-30 07:44:03 -0600

    add fish functions and config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,3 @@
 set -gx PATH $PATH ~/bin
 set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
+set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
---
+++ fish/.config/fish/functions/gslist.fish
@@ -0,0 +1,3 @@
+function gslist
+  rvm gemset list
+end
