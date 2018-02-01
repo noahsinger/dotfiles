891eb961e87295e288df450c9380d805d9f0a29d
Noah Singer <noah.singer@gmail.com>
2018-02-01 16:33:00 -0600

    Change referenced php config file

--- fish/.config/fish/functions/php56.fish
+++ fish/.config/fish/functions/php56.fish
@@ -1,3 +1,3 @@
 function php56
-  /usr/local/opt/php56/sbin/php-fpm --nodaemonize --fpm-config /usr/local/etc/php/5.6/fpm.d/fabricut-org.dev 
+  /usr/local/opt/php56/sbin/php-fpm --nodaemonize --fpm-config /usr/local/etc/php/5.6/php-fpm.conf 
 end
