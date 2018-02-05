6c03db648798085897aa1bfea36de8f3457a1e14
Noah Singer <noah.singer@gmail.com>
2018-02-05 09:48:27 -0600

    Add Pear path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,6 @@
 set -gx PATH $PATH ~/bin
+
 set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
+
 set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
+set -g fish_user_paths "/Users/nsinger/pear/bin" $fish_user_paths
