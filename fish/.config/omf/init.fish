8f54ba6db2dce4d56bd260c56d8dc7d239afebc1
Noah Singer <noah.singer@gmail.com>
2018-04-14 16:31:27 -0500

    delegate terminal colors to the terminal program

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -1,27 +1,2 @@
-# # Example bobthefish color scheme:
-set -g theme_color_scheme user
-#    
-set -g __color_initial_segment_exit  ffffff ce000f --bold
-set -g __color_initial_segment_su    ffffff 189303 --bold
-set -g __color_initial_segment_jobs  ffffff 255e87 --bold
-#    
-set -g __color_path                  333333 999999
-set -g __color_path_basename         333333 ffffff --bold
-set -g __color_path_nowrite          660000 cc9999
-set -g __color_path_nowrite_basename 660000 cc9999 --bold
-#    
-set -g __color_repo                  addc10 0c4801
-set -g __color_repo_work_tree        333333 ffffff --bold
-set -g __color_repo_dirty            99000f ffffff
-set -g __color_repo_staged           f6b117 3a2a03
-#    
-set -g __color_vi_mode_default       999999 333333 --bold
-set -g __color_vi_mode_insert        189303 333333 --bold
-set -g __color_vi_mode_visual        f6b117 3a2a03 --bold
-#    
-set -g __color_vagrant               48b4fb ffffff --bold
-set -g __color_username              cccccc 255e87 --bold
-set -g __color_hostname              cccccc 255e87
-set -g __color_rvm                   000099 cccccc --bold
-set -g __color_virtualfish           005faf cccccc --bold
-set -g __color_virtualgo             005faf cccccc --bold
+# delagate colors to the terminal program 
+set -g theme_color_scheme terminal
