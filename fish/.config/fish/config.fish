b11e0a1539fc22c13685146b0cdfffc24962a6b6
Noah Singer <noah.singer@gmail.com>
2018-04-14 17:33:46 -0500

    don't cause errors when paths aren't present

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,10 @@ set -gx PATH $PATH ~/bin
 
 set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
 
-set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
-set -g fish_user_paths "/Users/nsinger/pear/bin" $fish_user_paths
+if test -d /usr/local/opt/imagemagick@6/bin
+  set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
+end
+
+if test -d /Users/nsinger/pear/bin
+  set -g fish_user_paths "/Users/nsinger/pear/bin" $fish_user_paths
+end
