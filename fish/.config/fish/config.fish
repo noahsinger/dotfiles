701e9d6d0e8b217913728615e664fac25a930d9d
Noah Singer <noah.singer@gmail.com>
2019-10-15 10:31:06 -0500

    Udpate fish paths

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,3 +11,17 @@ if test -d /Users/nsinger/pear/bin
 end
 
 set -gx PATH /Users/nsinger/.rvm/gems/ruby-2.4.2/bin $PATH
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/httpie" $fish_user_paths
+
+set PATH /usr/local/bin $PATH
+
+set -g fish_user_paths "/usr/local/opt/node@8/bin" $fish_user_paths
+set -gx LDFLAGS "-L/usr/local/opt/node@8/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/node@8/include"
+
+set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
